a78dca67721ded5e265f8f4bdcd590c3825bea24
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-06-21 22:01:16 +0100

    adds prisma client

--- src/app/api/canvas/route.ts
+++ src/app/api/canvas/route.ts
@@ -2,8 +2,6 @@ import { NextResponse } from "next/server"
 import { createClient } from '@/utils/supabase/server'
 import { cookies } from "next/headers";
 
-//const supabase = createClient(cookies())
-
 export async function POST(req: Request, res: Response) {
   try{
     const body = await req.json()
